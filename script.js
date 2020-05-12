aa0ab2f5b36345979953745b5866295e706f119d
Keracy <fazzzle.de@gmail.com>
2020-05-13 01:26:40 +0300

    Добавил комментарии

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function encrypt(){
+function encrypt(){ //Зашифровать сообщение
   let nonEncryptedText = document.getElementById("encrypt").value;
-  let temp,encryptedText = "";
+  let temp,encryptedText = ""; //temp - переменная, которая хранит код символа,
+                              //после изменения кода записывает в encryptedText
   for(let i = 0;i < nonEncryptedText.length; i++){
-    temp = nonEncryptedText.codePointAt(i);
-    temp += Number(document.getElementById("encryptKey").value);
-    encryptedText += String.fromCodePoint(temp);
+    temp = nonEncryptedText.codePointAt(i);//Присваиваю код символа
+    temp += Number(document.getElementById("encryptKey").value);//Увеличить код символа на "ключ шифрования"
+    encryptedText += String.fromCodePoint(temp);//записать получившийся символ
   }
   alert(encryptedText);
 }
 
-function decrypt(){
+function decrypt(){ //Расшифровать работает по тому же принципу
   let nonDecryptedText = document.getElementById("decrypt").value;
   let temp,decryptedText = "";
   for(let i = 0;i < nonDecryptedText.length; i++){
